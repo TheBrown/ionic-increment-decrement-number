68f4839808ace82f305c874cfa26247962e3edee
Saleumsack <saleumsack16@gmail.com>
2018-09-11 14:30:13 +0700

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  qty: any;
 
   constructor(public navCtrl: NavController) {
-
+    this.qty = 1;
   }
 
+  incrementQty() {
+    this.qty += 1;
+  }
+
+  decrementQty() {
+    if (this.qty - 1 < 1) {
+      this.qty = 1;
+    } else {
+
+    this.qty -= 1;
+    }
+  }
 }
